4440210d5efc99b3c0a95cbbb510cbd318e7a6c6
marcosroman <marcosdromand@gmail.com>
2022-12-30 20:27:06 -0300

    funciona de forma funcional, todavia sin estado

--- src/PersonCard.js
+++ src/PersonCard.js
@@ -1,44 +1,23 @@
 import React from 'react'
 
-class PersonCard extends React.Component {
-  constructor(props) {
-    super(props);
-		this.state = {age: this.props.age}
-
-		//this.increaseAge = this.increaseAge.bind(this);
-	}
-
-	// de esto no tenia idea!!! pro eso no funciona
-    // This binding is necessary to make `this` work in the callback    this.handleClick = this.handleClick.bind(this);
-	//
-	
+const PersonCard = props => {
 	/*
-	increaseAge() {
-		this.setState(prevState => ({
-			age: prevState.age+1
-		}));
-	}
-	*/
-
-	increaseAge = () => {
+	const increaseAge = () => {
 		this.setState((state,props) => ({
 			age: this.state.age + 1
 		}));
 	}
+	*/
 
-	// both work! (second one works without the above binding, me like es prettier)
-	
-	render() {
-		return (
-			<div>
-				<h1>{this.props.lastName}, {this.props.firstName}</h1>
-				<p>Age: {this.state.age}</p>
-				<p>Hair color: {this.props.hairColor}</p>
-				<button onClick={this.increaseAge}>Birthday button for {this.props.firstName} {this.props.lastName}</button>
-			</div>
-		)
-	}
+	return (
+		<div>
+			<h1>{props.lastName}, {props.firstName}</h1>
+			<p>Age: {props.age}</p>
+			<p>Hair color: {props.hairColor}</p>
+		</div>
+	)
 }
 
+//<button onClick={this.increaseAge}>Birthday button for {this.props.firstName} {this.props.lastName}</button>
 export default PersonCard;
 
